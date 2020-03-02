9aa04b4b8c0d13b239306295b9336dfe164ebc52
aposipa <andrew.posipanka@gmail.com>
2020-03-02 13:50:11 -0500

    Created the basic login form on the login tab with a button link to a profile page.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Link } from "react-router-dom";
 
 
 const Login = (props) => {
   return (
 <div>
-
+    <Form className="login-form">
+      <FormGroup>
+        <Label>Email</Label>
+        <Input type="email" placeholder="Email"/>
+        <Label>Password</Label>
+        <Input type="password" placeholder="Password"/>
+      </FormGroup>
+      <Button>
+        <Link to="/profile">
+        Log in
+        </Link>
+      </Button>
+    </Form>
 </div>
   );
 }
